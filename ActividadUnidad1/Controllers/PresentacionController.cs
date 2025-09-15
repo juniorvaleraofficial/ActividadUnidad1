0785f1a0e0c000c4d3e55cb300397704826ddafd
Junior Valera <detoprox@gmail.com>
2025-09-14 22:57:50 -0400

    Agregar archivos de proyecto.

---
+++ ActividadUnidad1/Controllers/PresentacionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadUnidad1.Controllers
+{
+    public class PresentacionController : Controller
+    {
+        public IActionResult Saludo()
+        {
+            return View();
+        }
+    }
+}
