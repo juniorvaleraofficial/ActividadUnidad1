5def6bfa003f73dff594d1dc7bdb849c233527a8
Junior Valera <detoprox@gmail.com>
2025-09-15 16:55:15 -0400

    Configurando con ViewBag Mejores practicas.

--- ActividadUnidad1/Controllers/PresentacionController.cs
+++ ActividadUnidad1/Controllers/PresentacionController.cs
@@ -6,6 +6,9 @@ namespace ActividadUnidad1.Controllers
     {
         public IActionResult Saludo()
         {
+            ViewBag.Nombre = "Junior Alexis";
+            ViewBag.Apellido = "Valera Rijo";
+            ViewBag.Matricula = "MT-2023-00518";
             return View();
         }
     }
