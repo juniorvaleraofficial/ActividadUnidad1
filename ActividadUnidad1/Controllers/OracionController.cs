7fb3cefcae272190614b54eb075b8417631709e3
Junior Valera <detoprox@gmail.com>
2025-09-15 18:01:47 -0400

    OracionController con foreach

---
+++ ActividadUnidad1/Controllers/OracionController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+
+namespace ActividadUnidad1.Controllers
+{
+    public class OracionController : Controller
+    {
+        public IActionResult ContarVocales()
+        {
+            string frase = "Hola";
+            int contador = 0;
+            string[] vocales = { "a", "e", "i", "o", "u" };
+
+            foreach (char letra in frase.ToLower()) // recorremos la frase en minúsculas
+            {
+                if (vocales.Contains(letra.ToString())) // verificamos si la letra está en el array
+                {
+                    contador++;
+                }
+            }
+
+            Console.WriteLine("Cantidad de vocales: " + contador);
+            return View();
+        }
+    }
+}
