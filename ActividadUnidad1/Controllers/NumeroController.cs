9f2237cb5bd9063949cd407671d0af1cd0171254
Junior Valera <detoprox@gmail.com>
2025-09-16 12:42:48 -0400

    Creando la Accion Signo con ViewBag

--- ActividadUnidad1/Controllers/NumeroController.cs
+++ ActividadUnidad1/Controllers/NumeroController.cs
@@ -4,6 +4,21 @@ namespace ActividadUnidad1.Controllers
 {
     public class NumeroController : Controller
     {
-        public IActionResult
+        public IActionResult Signo()
+        {
+            int numero = 0;
+            String resultado;
+
+            if (numero > 0)
+            {
+               resultado = "El numero es positivo: ";
+            }
+            else
+            {
+               resultado = "El numero es negativo: ";
+            }
+            ViewBag.Signo= resultado;
+            return View();
+        }
     }
 }
