fa9d6c43e0eb5589b3c3bcf93535ffbaf1c722e4
Junior Valera <detoprox@gmail.com>
2025-09-18 17:50:24 -0400

    Probando la suma

---
+++ ActividadUnidad1/Controllers/OperacionesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadUnidad1.Controllers
+{
+    public class OperacionesController : Controller
+    {
+        // Métodos de lógica (no acción)
+        private int SumarEnteros(int a, int b) => a + b;
+
+        //Operaciones/Sumar?n=5&n2=7
+        [HttpGet]
+        public IActionResult Sumar(int n = 0, int n2 = 0)
+        {
+            int resultado = SumarEnteros(n, n2);
+            ViewBag.N1 = n;
+            ViewBag.N2 = n2;
+            ViewBag.Resultado = resultado;
+            return View();
+        }
+
+        //Operaciones/Sumar/5/7
+        [HttpGet("Operaciones/Sumar/{n:int}/{n2:int}")]
+        public IActionResult SumarConRuta(int n, int n2) => Sumar(n, n2);
+    }
+}
