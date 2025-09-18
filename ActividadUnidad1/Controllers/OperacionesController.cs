cc2d98f391114f9b51134556a3f31610a8782da9
Junior Valera <detoprox@gmail.com>
2025-09-18 18:01:40 -0400

    Probando la resta

--- ActividadUnidad1/Controllers/OperacionesController.cs
+++ ActividadUnidad1/Controllers/OperacionesController.cs
@@ -6,6 +6,7 @@ namespace ActividadUnidad1.Controllers
     {
         // Métodos de lógica (no acción)
         private int SumarEnteros(int a, int b) => a + b;
+        private int RestarEnteros(int a, int b) => a - b;
 
         //Operaciones/Sumar?n=5&n2=7
         [HttpGet]
@@ -21,5 +22,15 @@ namespace ActividadUnidad1.Controllers
         //Operaciones/Sumar/5/7
         [HttpGet("Operaciones/Sumar/{n:int}/{n2:int}")]
         public IActionResult SumarConRuta(int n, int n2) => Sumar(n, n2);
+
+        public IActionResult Restar(int n = 0, int n2 = 0)
+        {
+            int resultado = RestarEnteros(n, n2);
+            ViewBag.N1 = n;
+            ViewBag.N2 = n2;
+            ViewBag.Resultado = resultado;
+            return View();
+        }
+
     }
 }
