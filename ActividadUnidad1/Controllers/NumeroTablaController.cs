244579a455c57a1ec18a76017135507322555846
Junior Valera <detoprox@gmail.com>
2025-09-18 00:14:01 -0400

    Ejercicio 5 primer intento error de logica

---
+++ ActividadUnidad1/Controllers/NumeroTablaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActividadUnidad1.Controllers
+{
+    public class NumeroTablaController : Controller
+    {
+        [HttpGet("NumeroTabla/Tabla/{a:int}")]
+        public IActionResult Tabla(int a = 0)
+        {
+            int resultado = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = a * i;
+            }
+            ViewBag.resultado = resultado;
+            return View();
+        }
+    }
+}
