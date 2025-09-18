280fea0ab69ccd4a393b1eebf3c358a624adfbb1
Junior Valera <detoprox@gmail.com>
2025-09-18 12:44:58 -0400

    Convertir de f a c

---
+++ ActividadUnidad1/Controllers/TemperaturaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ActividadUnidad1.Controllers
+{
+    public class TemperaturaController : Controller
+    {
+        public IActionResult Convertir(double f = 0)
+        {
+            double c = f - 32 / 1.8;
+            double convertir = f;
+            ViewBag.Fahrenheit = c;
+            ViewBag.Convertir = convertir;
+            return View();
+        }
+    }
+}
