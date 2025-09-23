e2ef1298d6cc0e67f9734aba89613db3a45b7341
Junior Valera <detoprox@gmail.com>
2025-09-23 11:47:34 -0400

    Acción Usuario que recibe un correo como parámetro

---
+++ ActividadUnidad1/Controllers/CorreoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace ActividadUnidad1.Controllers
+{
+    public class CorreoController : Controller
+    {
+       
+        public ActionResult Usuario(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                return Content("No se recibió un correo válido.");
+            }
+
+            
+        }
+    }
+}
