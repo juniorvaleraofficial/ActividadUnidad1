519538aed4919366ea31dd5e1c7adf5b989cacc2
Junior Valera <detoprox@gmail.com>
2025-09-23 12:59:24 -0400

    aún no hay evaluación

---
+++ ActividadUnidad1/Controllers/SeguridadController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace ActividadUnidad1.Controllers
+{
+    public class SeguridadController : Controller
+    {
+        [HttpGet]
+        public ActionResult ClaveFuerte()
+        {
+            ViewBag.Resultado = null;
+            return View();
+        }
+    }
+}
